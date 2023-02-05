b7139f76062c21c0622d9bd59711d1ef07e1750c
Armando <armando.ortiz.jr.19@outlook.com>
2023-02-05 12:58:05 -0600

    Throw error if try to place ship on occupied space

--- src/Gameboard/gameboard.js
+++ src/Gameboard/gameboard.js
@@ -38,14 +38,55 @@ const GameBoard = () => {
     }
     return ship;
   };
+  const spacedOccupied = (length, coords, axis) => {
+    let coordX = coords[0];
+    let coordY = coords[1];
+    if (axis === 'x') {
+      const finalCoordX = coordX + length - 1;
+      while (coordX <= finalCoordX) {
+        if (grid[coordX][coordY][0] === 'o') {
+          return true;
+        }
+        coordX += 1;
+      }
+    } else if (axis === 'y') {
+      const finalCoordY = coordY + length - 1;
+      while (coordY <= finalCoordY) {
+        if (grid[coordX][coordY][0] === 'o') {
+          return true;
+        }
+        coordY += 1;
+      }
+    }
+    return false;
+  };
   const placeShip = (length, coords, axis = 'x') => {
+    console.log(`truthiness: ${spacedOccupied(length, coords, axis)}`);
+    if (spacedOccupied(length, coords, axis)) {
+      console.log('entrada');
+      throw new Error('Space is already occupied');
+    }
     const newShip = Ship(length);
     const updatedShip = markGridAsOccupied(length, coords, axis, newShip);
     ships.push(updatedShip);
     return updatedShip;
   };
 
-  return { grid, ships, placeShip };
+  const receiveAttack = (coords) => {
+    for (let i = 0; i < ships.length; i += 1) {
+      const ship = ships[i];
+      for (let j = 0; j < ship.shipCoords; j += 1) {
+        const shipCoord = ship.shipCoords[j];
+        if (coords === shipCoord) {
+          ship.hit();
+        }
+      }
+    }
+  };
+
+  return {
+    grid, ships, placeShip, receiveAttack,
+  };
 };
 
 module.exports = GameBoard;
--- src/Gameboard/gameboard.test.js
+++ src/Gameboard/gameboard.test.js
@@ -24,3 +24,23 @@ test('Gameboard placeShip marks coordinates as occupied', () => {
   expect(board.grid[1][0]).toEqual(['o']);
   expect(board.grid[2][0]).not.toEqual(['o']);
 });
+
+test('Gameboard placeShip throws an error if coordinates are already occupied', () => {
+  const board = GameBoard();
+  board.placeShip(2, [0, 0], 'x');
+  board.placeShip(3, [7, 7], 'y');
+
+  expect(() => board.placeShip(1, [0, 0])).toThrow('Space is already occupied');
+  expect(() => board.placeShip(2, [7, 8], 'y')).toThrow('Space is already occupied');
+});
+
+test('Gameboard has receiveAttack function', () => {
+  expect(typeof GameBoard().receiveAttack).toBe('function');
+});
+
+/*test('Gameboard receiveAttack function calls hit if it hits a ship', () => {
+  const board = GameBoard();
+  board.placeShip(4, [2, 0], 'x');
+  board.receiveAttack([5, 0]);
+  expect(board.ships[0].hitNum).toBe(1);
+});*/
